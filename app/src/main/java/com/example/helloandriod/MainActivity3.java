9c91a0b195077718f01c0f2e8a55bf4cf0eaac16
Richard D Collamat <Richardcollamat34@gmail.com>
2023-03-04 13:15:45 +0800

    new added2

--- app/src/main/java/com/example/helloandriod/MainActivity3.java
+++ app/src/main/java/com/example/helloandriod/MainActivity3.java
@@ -8,22 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
         Log.v(TAG, "hello world");
 
-        Button button = (Button) findViewById(R.id.button2);
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 Log.i(TAG, " Button Clicked");
-                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                Intent intent = new Intent(MainActivity3.this, MainActivity2.class);
                 startActivity(intent);
             }
         });
